29f4cf68ec2e474a3d242bb759463bc441f9035a
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-23 18:59:12 +0000

    Funcionando

--- Cifrado-de-Vigenere/cifradoVigenere.py
+++ Cifrado-de-Vigenere/cifradoVigenere.py
@@ -6,8 +6,7 @@ def ToString(s):
      
     return str
 
-abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']
+abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']
 
 texto = input("Introduzca el texto: ")
 texto = texto.replace(" ", "")
