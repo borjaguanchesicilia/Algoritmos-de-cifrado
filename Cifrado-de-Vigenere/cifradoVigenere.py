c3ea7450360a1c5b069c0dae5925295a62bd44f3
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-24 13:39:06 +0000

    Descifrado

--- Cifrado-de-Vigenere/cifradoVigenere.py
+++ Cifrado-de-Vigenere/cifradoVigenere.py
@@ -3,6 +3,8 @@
 # Date: 23/02/2021
 # File cifradoVigenere: Implementación del cifrado de Vigenère.
 
+import sys
+
 abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']
 
 texto = input("Introduzca el texto: ")
@@ -46,3 +48,49 @@ for i in range(len(cifradoNum)):
             cifrado = cifrado + abc[j]
 
 print("El texto cifrado es: ", cifrado)
+
+resp = input("¿Quiere descifrar el mensaje? S/n")
+
+if resp == 'S' or resp == 's':
+
+    textoCifradoNum = []
+    for i in range(len(cifrado)):
+        for j in range(len(abc)):
+            if (cifrado[i] == abc[j]):
+                textoCifradoNum.append(j)
+
+    print("El texto cifrafo con números es: ", textoCifradoNum)
+
+
+    desCifradoNum = []
+    cont = num = 0
+    for i in range(len(textoCifradoNum)):
+        if (cont+1 == tam):
+            num = (textoCifradoNum[i] - claveNum[cont]) % 26
+            if num < 0:
+                num = num + 26
+                
+            desCifradoNum.append(num)
+            cont = 0
+        else:
+            num = (textoCifradoNum[i] - claveNum[cont]) % 26
+            if num < 0:
+                num = num + 26
+                
+            desCifradoNum.append(num)
+            cont += 1
+    
+    print("El texto descifrafo con números es: ", desCifradoNum)
+
+
+    desCifrado = ""
+    for i in range(len(desCifradoNum)):
+        for j in range(len(abc)):
+            if (desCifradoNum[i] == j):
+                desCifrado = desCifrado + abc[j]
+
+    print("El texto descifrado es: ", desCifrado)
+
+else:
+    print("Saliendo del programa...")
+    sys.exit()
