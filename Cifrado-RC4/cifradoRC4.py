a098d5a0f92c8371d58a3c76e48982b9bc3a199d
Borja Guanche Sicilia <55703443+borjaguanchesicilia@users.noreply.github.com>
2021-03-07 19:35:45 +0000

    Update cifradoRC4.py

--- Cifrado-RC4/cifradoRC4.py
+++ Cifrado-RC4/cifradoRC4.py
@@ -1,60 +1,35 @@
-# Author: Borja Guanche Sicilia
-# Mail: alu0101205908@ull.edu.es
-# # Date: 01/03/2021
-# File cifradoRC4.py: Implementación del cifrado RC4.
-
-semilla = [2, 5]
-texto = [1, 34]
-S = []
-K = []
-for i in range(256):
-    S.append(i)
-    K.append(semilla[i % len(semilla)])
+ return textoCifrado
 
 
-j = 0
-for i in range(256):
-    j = ((j + S[i] + K[i]) % 256)
-    S[i], S[j] = S[j], S[i]
+def desCifrar(textoCifrado, secuenciaCifrante):
+    textoDescifrado = []
+    for i in range(len(textoCifrado)):
+        textoDescifrado.append(int(textoCifrado[i], 2) ^ int(secuenciaCifrante[i], 2))
 
-print("S: ", S)
-print("\nK: ", K)
-
-
-secuaciaCifrante = []
-k = i = j = 0
-while(k < len(texto)):
-    i = (i + 1) % 256
-    j = (j+S[i]) % 256
-    S[i], S[j] = S[j], S[i]
-    t = (S[i]+S[j]) % 256
-    secuaciaCifrante.append(t)
-    k += 1
+    return textoDescifrado
 
 
-for i in range(len(secuaciaCifrante)):
-    secuaciaCifrante[i] = S[secuaciaCifrante[i]]
-    secuaciaCifrante[i] = str(bin(secuaciaCifrante[i])[2:]) 
-
-print("\nSecuancia cifrante: ", secuaciaCifrante)
+def error():
+    print("Saliendo del programa...")
+    sys.exit()
 
 
-textoBinario = []
-for i in range(len(texto)):
-    textoBinario.append(str(bin(texto[i])[2:]))
+res = input("¿Quiere cifrar el mensaje? S/n  ")
 
-print("\nTexto original en binario", textoBinario)
-
-
-textoCifrado = []
-for i in range(len(textoBinario)):
-    textoCifrado.append(int(bin(int(textoBinario[i], 2) ^ int(secuaciaCifrante[i], 2))[2:]))
-
-print("\nTexto cifrado", textoCifrado)
-
-
-textoDesifrado = []
-for i in range(len(textoCifrado)):
-    textoDesifrado.append(int(textoCifrado[i], 2) ^ int(secuaciaCifrante[i], 2))
-
-print("\nTexto descifrado", textoDesifrado)
+if res == 'S' or res == 's':
+    semilla = [2, 5]
+    texto = [1, 34]
+    S = ksa(semilla)
+    generacion = prga(S, texto)
+    textoBinario = pasarBinario(texto)
+    print("\nEl texto es: ", texto)
+    print("\nLa semilla es: ", semilla)
+    print("\nEl texto cifrado es: ", cifrar(textoBinario, generacion))
+elif res == 'N' or res == 'n':
+    semilla = ['10010000', '1110']
+    textoCifrado = ['10010001', '101100']
+    print("\nEl texto cifrado es: ", textoCifrado)
+    print("\nLa semilla es: ", semilla)
+    print("\nEl texto descifrado es: ", desCifrar(textoCifrado, semilla))
+else:
+    error()
