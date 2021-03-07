fd9960d422061cf4e20a52583458abcab05cf90c
Borja Guanche Sicilia <55703443+borjaguanchesicilia@users.noreply.github.com>
2021-03-07 19:36:09 +0000

    Update cifradoRC4.py

--- Cifrado-RC4/cifradoRC4.py
+++ Cifrado-RC4/cifradoRC4.py
@@ -1,4 +1,56 @@
- return textoCifrado
+# Author: Borja Guanche Sicilia
+# Mail: alu0101205908@ull.edu.es
+# # Date: 01/03/2021
+# File cifradoRC4.py: Implementación del cifrado RC4.
+
+
+def ksa(semilla):
+    S = []
+    K = []
+    j = 0
+    for i in range(256):
+        S.append(i)
+        K.append(semilla[i % len(semilla)])
+
+    for i in range(256):
+        j = ((j + S[i] + K[i]) % 256)
+        S[i], S[j] = S[j], S[i]
+
+    return S
+
+
+def prga(S, texto):
+    secCifr = []
+    k = i = j = 0
+    while(k < len(texto)):
+        i = (i + 1) % 256
+        j = (j+S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        t = (S[i]+S[j]) % 256
+        secCifr.append(t)
+        k += 1
+
+    for i in range(len(secCifr)):
+        secCifr[i] = S[secCifr[i]]
+        secCifr[i] = str(bin(secCifr[i])[2:]) 
+
+    return secCifr
+
+
+def pasarBinario(texto):
+    textoBinario = []
+    for i in range(len(texto)):
+        textoBinario.append(str(bin(texto[i])[2:]))
+
+    return textoBinario
+
+
+def cifrar(textoBinario, secuenciaCifrante):
+    textoCifrado = []
+    for i in range(len(textoBinario)):
+        textoCifrado.append(bin(int(textoBinario[i], 2) ^ int(secuenciaCifrante[i], 2))[2:])
+
+    return textoCifrado
 
 
 def desCifrar(textoCifrado, secuenciaCifrante):
