6f3a75ca11e05c85425b969c76e1a549f4dd5129
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-03-14 13:14:14 +0000

    Organizado y comentado

--- Cifrado-Chacha20/chacha20.py
+++ Cifrado-Chacha20/chacha20.py
@@ -1,3 +1,4 @@
+# Función para relizar las operaciones de suma (en módulo 32), xor y desplazamiento
 def qr(a,b,c,d):
 
     a = int(a, 16)
@@ -7,25 +8,26 @@ def qr(a,b,c,d):
 
     a = (a + b)&0xffffffff
     d = d ^ a
-    d = rotl(d, 16)
+    d = rotacionAIzquierda(d, 16)
 
     c = (c + d)&0xffffffff
     b = b ^ c
-    b = rotl(b, 12)
+    b = rotacionAIzquierda(b, 12)
 
     a = (a + b)&0xffffffff
     d = d ^ a
-    d = rotl(d, 8)
+    d = rotacionAIzquierda(d, 8)
 
     c = (c + d)&0xffffffff
     b = b ^ c
-    b = rotl(b, 7)
+    b = rotacionAIzquierda(b, 7)
 
     return hex(a), hex(b), hex(c), hex(d)
 
 
-def rotl(a, b):
-    return ((a << b)&0xffffffff) | (a >> (32 - b))
+# Función para realizar el desplazamiento a izquierda de n bits
+def rotacionAIzquierda(a, n):
+    return ((a << n)&0xffffffff) | (a >> (32 - n))
 
 
 # Función para convertir una string en un hexadecimal en formato little endian
@@ -40,16 +42,20 @@ def toLittleEndian(str):
     littleEndian = str[9] + littleEndian
     littleEndian = "0x" + littleEndian
 
-    num = int(littleEndian, 16)
-    return hex(num)
+    return hex(int(littleEndian, 16))
 
 
-def inner_Block(x):
+# Función para realizar las operaciones qr en columnas y diagonales
+def bloqueInterno(x):
+
+    # Operaciones sobre columnas:
 
     x[0], x[4], x[8], x[12] = qr(x[0], x[4], x[8], x[12])
     x[1], x[5], x[9], x[13] = qr(x[1], x[5], x[9], x[13])
     x[2], x[6], x[10], x[14] = qr(x[2], x[6], x[10], x[14])
     x[3], x[7], x[11], x[15] = qr(x[3], x[7], x[11], x[15])
+
+    # Operaciones sobre diagonales:
 
     x[0], x[5], x[10], x[15] = qr(x[0], x[5], x[10], x[15])
     x[1], x[6], x[11], x[12] = qr(x[1], x[6], x[11], x[12])
@@ -59,10 +65,18 @@ def inner_Block(x):
     return x
 
 
-def chacha20Block(clave, contador, nonce):
+def mostrarEstado(x):
+    print(x[0][2:], x[1][2:], x[2][2:], x[3][2:])
+    print(x[4][2:], x[5][2:], x[6][2:], x[7][2:])
+    print(x[8][2:], x[9][2:], x[10][2:], x[11][2:])
+    print(x[12][2:], x[13][2:], x[14][2:], x[15][2:], "\n\n")
+
+
+def BloqueChacha20(clave, contador, nonce):
     x = []
     entrada = []
     salida = []
+    descifrado = []
 
     #Constantes (128 bits) 4 palabras
     x.append(hex(0x61707865))
@@ -92,33 +106,25 @@ def chacha20Block(clave, contador, nonce):
 
    
     print("Estado inicial:\n")
-    print(x[0][2:], x[1][2:], x[2][2:], x[3][2:])
-    print(x[4][2:], x[5][2:], x[6][2:], x[7][2:])
-    print(x[8][2:], x[9][2:], x[10][2:], x[11][2:])
-    print(x[12][2:], x[13][2:], x[14][2:], x[15][2:], "\n\n")
+    mostrarEstado(x)
 
     for i in range(10):
-        inner_Block(x)
+        bloqueInterno(x)
         
 
     print("Estado final tras las 20 iteraciones:\n")
-    print(x[0][2:], x[1][2:], x[2][2:], x[3][2:])
-    print(x[4][2:], x[5][2:], x[6][2:], x[7][2:])
-    print(x[8][2:], x[9][2:], x[10][2:], x[11][2:])
-    print(x[12][2:], x[13][2:], x[14][2:], x[15][2:], "\n\n")
+    mostrarEstado(x)
+
 
     for i in range(16):
         salida.append(hex(int(x[i],16) + int(entrada[i],16)))
 
     print("Estado de salida del generador:\n")
-    print(salida[0][2:], salida[1][2:], salida[2][2:], salida[3][2:])
-    print(salida[4][2:], salida[5][2:], salida[6][2:], salida[7][2:])
-    print(salida[8][2:], salida[9][2:], salida[10][2:], salida[11][2:])
-    print(salida[12][2:], salida[13][2:], salida[14][2:], salida[15][2:])
+    mostrarEstado(salida)
 
 
 clave = ['00:01:02:03', '04:05:06:07','08:09:0a:0b', '0c:0d:0e:0f', '10:11:12:13', '14:15:16:17', '18:19:1a:1b', '1c:1d:1e:1f']
 contador = ['01:00:00:00']
 nonce = ['00:00:00:09', '00:00:00:4a', '00:00:00:00']
 
-chachaBlock(clave, contador, nonce)
+BloqueChacha20(clave, contador, nonce)
