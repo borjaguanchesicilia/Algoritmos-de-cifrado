cc0192238cf7b53aabb47ba54346136229fc4975
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-04-09 20:27:00 +0100

    Recogida de datos

--- Cifrado-AES/main.py
+++ Cifrado-AES/main.py
@@ -20,11 +20,7 @@ def main(claveString, textoEnClaroString):
 
     aes(clave, textoEnClaroString)
 
-#clave = input("Introduzca la clave: ")
-#textoEnClaro = input("Introduzca el texto en claro: ")
-clave = "000102030405060708090a0b0c0d0e0f"
-textoEnClaro = "00112233445566778899aabbccddeeff"
-#clave = "2b7e151628aed2a6abf7158809cf4f3c"
-#textoEnClaro = "3243f6a8885a308d313198a2e0370734"
+clave = input("Introduzca la clave: ")
+textoEnClaro = input("Introduzca el texto en claro: ")
 
 main(clave, textoEnClaro)
