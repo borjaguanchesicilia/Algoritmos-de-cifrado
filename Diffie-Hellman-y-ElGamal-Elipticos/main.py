b69608eb873f069a0dc77eeb6cd5abffc859c182
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-05-12 23:13:22 +0100

    Recogida de datos

--- Diffie-Hellman-y-ElGamal-Elipticos/main.py
+++ Diffie-Hellman-y-ElGamal-Elipticos/main.py
@@ -6,18 +6,19 @@ def main():
     ficheroExpoRapida = open("exponenciacionRapida.txt", "w")
     ficheroEuclidesExten = open("euclidesExtendido.txt", "w")
 
-    p = 13
-    a = 5
-    b = 3
-    G = [9, 6]
-    da = 4
-    db = 2
-    m = 2
+    p = int(input("Introduzca el número primo: "))
+    a = int(input("Introduzca la cte a de la curva: "))
+    b = int(input("Introduzca la cte b de la curva: "))
+    p0 = int(input("Introduzca la primera coordenada del punto base: "))
+    p1 = int(input("Introduzca la segunda coordenada del punto base: "))
+    da = int(input("Introduzca el secreto de Alice: "))
+    db = int(input("Introduzca el secreto de Bob: "))
+    m = int(input("Introduzca el mensaje a cifrar: "))
 
     if (primos(p, ficheroExpoRapida) == True): # Aplicamos el test de primalidad a p para saber si es primo
         if (4*(a**3)+27*(b**2) != 0): # 4a³+27b² != 0
-            if (((G[1]**2)%p) == ((G[0]**3)+a*G[0]+b)%p): # Comprobamos si el punto pertenece a la curva
-                elGamalEliptico(p, a, b, G, da, db, m, ficheroEuclidesExten)
+            if (((p1**2)%p) == ((p0**3)+a*p0+b)%p): # Comprobamos si el punto pertenece a la curva
+                elGamalEliptico(p, a, b, [p0, p1], da, db, m, ficheroEuclidesExten)
             else:
                 print(f'El punto base {G[0], G[1]} no pertenece a la curva.')
         else:
